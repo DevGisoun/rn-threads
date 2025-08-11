753d5a16a69a568eda3ec6a1b3cb7197c62b08c7
DevGisoun <devgisoun@gmail.com>
2025-08-11 01:34:00 +0000

    [feat] 'New Post' 레이아웃 구성.
    - "softwareKeyboardLayoutMode": "resize" 옵션 추가.
      - 안드로이드 네이티브에서 앱의 전체 화면 크기를 다시 조정하는 옵션.

--- app/(protected)/new.tsx
+++ app/(protected)/new.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+    KeyboardAvoidingView,
+    Platform,
+    Pressable,
+    Text,
+    TextInput,
+    View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function New() {
+    const [text, setText] = useState<string>('');
+
     return (
-        <View>
-            <Text>New</Text>
-        </View>
+        <SafeAreaView edges={['bottom']} className="p-4 flex-1">
+            <KeyboardAvoidingView
+                className="flex-1"
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                keyboardVerticalOffset={Platform.OS === 'ios' ? 140 : 0}
+            >
+                <Text className="text-white text-lg font-bold">username</Text>
+
+                <TextInput
+                    multiline
+                    placeholder="What is your mind?"
+                    placeholderTextColor={'#737373'}
+                    className="text-white text-lg"
+                    numberOfLines={4}
+                    value={text}
+                    onChangeText={setText}
+                />
+
+                <View className="mt-auto">
+                    <Pressable
+                        onPress={() => {}}
+                        className="self-end p-3 px-6 bg-white rounded-full"
+                    >
+                        <Text className="text-black font-bold">Post</Text>
+                    </Pressable>
+                </View>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
     );
 }
